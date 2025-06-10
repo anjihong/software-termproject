9d910f542f4925d276409ce64064f360667b9d80
hong <anjhu@pusan.ac.kr>
2025-06-11 03:27:06 +0900

    api 키 완성

--- app/src/main/java/com/example/gittest/network/VisionApiClient.kt
+++ app/src/main/java/com/example/gittest/network/VisionApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object VisionApiClient {
     private const val BASE_URL = "https://vision.googleapis.com/"
-    const val API_KEY = "AIzaSyDMirgaJb3U7UKFISasAVe0hfqXA6fpKs4"  // 발급받은 키를 여기에 넣으세요
+    const val API_KEY = "AIzaSyCooOi2akuVUM59i2ZFsEYXNhgdlMhao3s"  // 발급받은 키를 여기에 넣으세요
 
     val service: VisionApiService by lazy {
         Retrofit.Builder()
